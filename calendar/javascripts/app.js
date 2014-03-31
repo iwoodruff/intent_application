b23ba42a31bed75ea06334803515bda2f0379673
iwoodruff <isaac.woodruff@gmail.com>
2014-03-30 20:56:34 -0400

    styling done, animations beginning

--- calendar/javascripts/app.js
+++ calendar/javascripts/app.js
@@ -1,29 +1,33 @@
 var CalendarDate = Backbone.Model.extend({
   next_month: function(){
+    var self = this
     var month = this.get('month')
+    var year = this.get('year')
     if (month == 11){
-      month = 0
+      month = 0;
+      year += 1;
+      self.set({'month': month, 'year': year})
     } else {
-      month += 1
+      month += 1;
+      self.set({'month': month})
     }
-    this.set('month', month)
-
   },
 
   prev_month: function(){
+    var self = this
     var month = this.get('month')
+    var year = this.get('year')
     if (month == 0){
-      month = 11
+      month = 11;
+      year -= 1
+      self.set({'month': month, 'year': year})
     } else {
-      month -= 1
+      month -= 1;
+      self.set({'month': month})
     }
-    this.set('month', month)
   },
 
   select_day: function(day){
-    console.log(day)
-    console.log('yo!')
-
     var clicked_day = day.getAttribute('id').split('-')
     var year = clicked_day[0]
     var month = clicked_day[1]
@@ -35,6 +39,9 @@ var CalendarDate = Backbone.Model.extend({
     this.set('date', date.getDate())
     this.set('month', date.getMonth())
     this.set('year', date.getYear() + 1900)
+
+    this.flip_page()
+
   }
 })
 
@@ -42,20 +49,14 @@ var MonthView = Backbone.View.extend({
   initialize: function(){
     this.listenTo(this.model, 'change', this.update)
     this.render()
+    this.removed_pages = 0
   },
 
   update: function(){
     this.calendar_body().innerHTML = '';
     this.calendar_sidebar().innerHTML = '';
-    this.current_month().innerHTML = '';
-    _.each (document.getElementsByClassName('calendar_button'), function(button){
-      button.innerHTML= '';
-    })
+    this.calendar_header().innerHTML = '';
     this.render();
-  },
-
-  calendar_nav: function(){
-    return document.getElementById('calendar_nav');
   },
 
   calendar_body: function(){
@@ -74,11 +75,8 @@ var MonthView = Backbone.View.extend({
     return document.getElementById('current_month');
   },
 
-  render_content: function(){
-    var self = this;
-    this.template_body(self.model.attributes);
-    this.template_sidebar(self.model.attributes);
-    this.template_current_month(self.model.attributes);
+  date_display: function(){
+    return document.getElementById('date_display')
   },
 
   render: function(){
@@ -90,19 +88,23 @@ var MonthView = Backbone.View.extend({
     _.each (model_proto, function(value, prop){
       if (prop == 'next_month') {
         var $next = document.createElement('div')
-        $next.innerHTML = '>'
+        $next.innerHTML = '<h3>></h3>'
         $next.id = 'next_month' 
         $next.className = 'calendar_button'
-        $next.addEventListener('click', function(){
+
+        $next.addEventListener('click', function(e){
+          e.preventDefault()
           self.model[prop]()
         })
         self.calendar_header().appendChild($next)
       } else if (prop == 'prev_month') {
         var $prev = document.createElement('div')
-        $prev.innerHTML = '<'
+        $prev.innerHTML = '<h3><</h3>'
         $prev.id = 'prev_month'
         $prev.className = 'calendar_button' 
-        $prev.addEventListener('click', function(){
+
+        $prev.addEventListener('click', function(e){
+          e.preventDefault()
           self.model[prop]()
         })
         self.calendar_header().appendChild($prev)
@@ -124,27 +126,63 @@ var MonthView = Backbone.View.extend({
 
     this.template_sidebar(self.model.attributes);
     this.template_current_month(self.model.attributes);
+
+    $('#switch_colors').on('click', function(){
+      if ($('#switch_colors').hasClass('clicked')){
+        $('#switch_colors').removeClass('clicked')
+        $('#calendar').css('opacity', 1.0).css('z-index', 100)
+        $('#source_img').css('opacity', 0.2).css('z-index', 10)
+        $('#switch_colors').prepend('<h2>MOCK-UP<h2>')
+      } else {
+        $('#switch_colors').addClass('clicked')
+        $('#calendar').css('opacity', 0.0).css('z-index', 1000)
+        $('#source_img').css('opacity', 1.0).css('z-index', 10)
+        $('#switch_colors').prepend('<h2>SOURCE<h2>')
+      }
+    })
   },
 
   template_sidebar: function(attrs){
     var self = this
+
     var $day_display = document.createElement('div')
     $day_display.id = 'day_display'
-    $day_display.innerHTML = '<h2 id="day_display">' + this.get_month(attrs.month) + '</h2>'
+    $day_display.innerHTML = '<h2 id="day_display">' + this.get_day(attrs.day) + '</h2>'
     self.calendar_sidebar().appendChild($day_display)
 
     var $date_display = document.createElement('div')
     $date_display.id = 'date_display'
     $date_display.innerHTML = '<h1 id="date_display">' + attrs.date + '</h1>'
     self.calendar_sidebar().appendChild($date_display)
+
+    var $page_one = document.createElement('div')
+    $page_one.id = 'page_one'
+    $page_one.className = 'day_pages visible'
+    self.date_display().appendChild($page_one)
+
+    var $page_two = document.createElement('div')
+    $page_two.id = 'page_two'
+    $page_two.className = 'day_pages visible'
+    self.date_display().appendChild($page_two)
+
+    var $page_three = document.createElement('div')
+    $page_three.id = 'page_three'
+    $page_three.className = 'day_pages visible'
+    self.date_display().appendChild($page_three)
+
+    var $ribbing = document.createElement('div')
+    $ribbing.id = 'ribbing'
+    $ribbing.className = 'ribbing'
+    self.calendar_sidebar().appendChild($ribbing)
   },
 
   template_current_month: function(attrs){
-    this.current_month().innerHTML = '<h3 id="current_month">'+ this.get_month(attrs.month) +'</h3>'
+    this.current_month().innerHTML = '<h3 id="current_month">'+ this.get_month(attrs.month)+' '+attrs.year+'</h3>'
   },
 
   template_body: function(attrs){
     var self = this
+
     var total_days = this.total_days(attrs.month, attrs.year)
     var current_month = this.get_month(attrs.month)
     var overflow = this.first_day(attrs.year, attrs.month)
@@ -167,7 +205,7 @@ var MonthView = Backbone.View.extend({
 
     for (i = overflow_days + 1; i <= prev_total; i++){
       // creates <td> for first week, including days carried over from past month
-      month_array.push('<td id="'+attrs.year+'-'+attrs.month+'-'+i+'" class="day"></td>')
+      month_array.push('<td id="'+attrs.year+'-'+(attrs.month - 1)+'-'+i+'" class="day"></td>')
     }
     for (i = 1; i <= (7 - overflow);  i++){
       // completes the first week with days from the current month
@@ -179,6 +217,7 @@ var MonthView = Backbone.View.extend({
       // creates <td> for the rest of the month, beginning where the prior ended
       var week_start = (7 * i) + (8 - overflow)
       var week_stop = (7 * (i + 1)) + (8 - overflow)
+
       // breaks the month into iteratable weeks
       if (week_stop <= total_days){
         // for all middle weeks
@@ -194,9 +233,9 @@ var MonthView = Backbone.View.extend({
           month_array.push('<td id="'+attrs.year+'-'+attrs.month+'-'+j+'" class="day">'+j+'</td>')
         }
 
-        for (i = 1; i < overflow_next_days; i++){
+        for (j = 1; j < overflow_next_days; j++){
           // creates <td> for the beginning of next month flowing into the current month
-          month_array.push('<td id="'+attrs.year+'-'+(attrs.month + 1)+'-'+i+'" class="day"></td>')
+          month_array.push('<td id="'+attrs.year+'-'+(attrs.month + 1)+'-'+j+'" class="day"></td>')
         }
         month_array.push('</tr></table>')
       }
@@ -216,8 +255,8 @@ var MonthView = Backbone.View.extend({
 
     var $today = document.getElementById(''+attrs.year+'-'+attrs.month+'-'+attrs.date+'')
 
-    $today.style.backgroundColor = 'red'
-    $today.style.color = 'white'
+    // $today.style.backgroundColor = '#E43737'
+    // $today.style.color = 'white'
   },
 
   first_day: function(year, month){
@@ -252,26 +291,30 @@ var MonthView = Backbone.View.extend({
 var CalendarView = Backbone.View.extend({
   initialize: function(){
     var self = this;
-    this.render()
-    this.views = []
+    this.render();
+    this.views = [];
   },
 
   render: function(){
     var self = this;
-    var today = new Date()
+    var today = new Date();
 
     var calendar_date = new CalendarDate({
       day: today.getDay(),
       date: today.getDate(),
       month: today.getMonth(),
       year: (today.getYear() + 1900),
-    })
+    });
 
     var month_view = new MonthView({
       model: calendar_date
     });
 
     self.$el.append(month_view.$el);
+
+    var $calendar_shadow = document.createElement('div')
+    $calendar_shadow.id = 'calendar_shadow'
+    document.getElementById('calendar').parentNode.appendChild($calendar_shadow)
 
   }
 
